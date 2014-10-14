60e414b36173233a468a0201277d48e97be8d507
Erik Agterdenbos <Erik Agterdenbos@s081153.campus.tue.nl>
2014-10-14 15:21:20 +0200

    refactor

--- src/termclustering/LuceneDatabase.java
+++ src/termclustering/LuceneDatabase.java
@@ -26,45 +26,37 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.util.Version;
 
-public class LuceneClustering {
+public class LuceneDatabase {
+	// based on http://www.lucenetutorial.com/lucene-in-5-minutes.html
+	
+	public Directory index;
+	public IndexWriterConfig config;
+	public StandardAnalyzer analyzer;
+	
+	public LuceneDatabase() throws SQLException, IOException, ParseException {	
+		analyzer = new StandardAnalyzer(Version.LUCENE_4_10_1);
+		index = new RAMDirectory();
+		config = new IndexWriterConfig(Version.LUCENE_4_10_1, analyzer);
+	}
 
-	public LuceneClustering() throws SQLException, IOException, ParseException {
-		// TODO Auto-generated constructor stub
-		
-		StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_4_10_1);
-		Directory index = new RAMDirectory();
-
-		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_1, analyzer);
-
-		Connection connection;
-		connection = DriverManager.getConnection("jdbc:mysql://178.62.207.179/2id26?"
-				+ "user=root&password=Aarde-Rond-1");
-		
-		Statement statement = connection.createStatement();
-		ResultSet resultSet = statement.executeQuery("SELECT id, text FROM lyric LIMIT 1000");
-		
-		IndexWriter w = new IndexWriter(index, config);
-		
-		while (resultSet.next()) {
-			int lyricId = resultSet.getInt("id");
-			String text = resultSet.getString("text").toLowerCase();
-			addLyric(w, text, lyricId);
-			System.out.println("Insert id " + lyricId + " text: " + text.length());
-		}
-		//addLyric(w, "Lucene in Action", "193398817");
-		//addLyric(w, "Lucene for Dummies", "55320055Z");
-		//addLyric(w, "Managing Gigabytes", "55063554A");
-		//addLyric(w, "The Art of Computer Science", "9900333X");
-		w.close();
+	public static void main(String[] args) throws SQLException, IOException, ParseException {
+		LuceneDatabase database = new LuceneDatabase();
+	
+		database.fillFromMysql();
 		
 	    // 2. query
 	    String querystr = "love";
 
 	    // the "title" arg specifies the default field to use
 	    // when no field is explicitly specified in the query.
-	    Query q = new QueryParser(Version.LUCENE_4_10_1, "lyric", analyzer).parse(querystr);
+	    
+	    Query q = new QueryParser(Version.LUCENE_4_10_1, "lyric", database.analyzer).parse(querystr);
+	    database.search(q);
+	}
 
-	    // 3. search
+	public void search(Query q)
+			throws IOException {
+		// 3. search
 	    int hitsPerPage = 10;
 	    IndexReader reader = DirectoryReader.open(index);
 	    IndexSearcher searcher = new IndexSearcher(reader);
@@ -85,9 +77,24 @@ public class LuceneClustering {
 	    reader.close();
 	}
 
-	public static void main(String[] args) throws SQLException, IOException, ParseException {
-		// TODO Auto-generated method stub
-		new LuceneClustering();
+	public void fillFromMysql()
+			throws SQLException, IOException {
+		Connection connection;
+		connection = DriverManager.getConnection("jdbc:mysql://178.62.207.179/2id26?"
+				+ "user=root&password=Aarde-Rond-1");
+		
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery("SELECT id, text FROM lyric LIMIT 1000");
+		
+		IndexWriter w = new IndexWriter(index, config);
+		
+		while (resultSet.next()) {
+			int lyricId = resultSet.getInt("id");
+			String text = resultSet.getString("text").toLowerCase();
+			addLyric(w, text, lyricId);
+			System.out.println("Insert id " + lyricId + " text: " + text.length());
+		}
+		w.close();
 	}
 	
 	private static void addLyric(IndexWriter w, String text, int lyricId) throws IOException {
