8e7915aa588fe3dcdb1aa83c9b21e768124fbf2c
Erik Agterdenbos <Erik Agterdenbos@s081153.campus.tue.nl>
2014-10-15 14:17:54 +0200

    clustering alleen met carrot, zonder lucene, resultaat naar lyricscluster en lyricsclustermember tabel in mysql

--- src/termclustering/LyricClustering.java
+++ src/termclustering/LyricClustering.java
@@ -2,6 +2,7 @@ package termclustering;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -30,12 +31,18 @@ public class LyricClustering {
 		Statement statement = connection.createStatement();
 		ResultSet resultSet = statement.executeQuery("SELECT id, text FROM lyric WHERE source='wikia' LIMIT 1000");
 		
+		PreparedStatement insertClusterStatement = connection
+		          .prepareStatement("INSERT INTO lyriccluster VALUES (?, ?, null)");
+		
+		PreparedStatement insertClusterMemberStatement = connection
+		          .prepareStatement("INSERT INTO lyricclustermember VALUES (?, ?)");
+		
 		while (resultSet.next()) {
-			int lyricId = resultSet.getInt("id");
+			int id = resultSet.getInt("id");
 			String text = resultSet.getString("text").toLowerCase();
-			Document doc = new Document(Integer.toString(lyricId), text);
+			Document doc = new Document(Integer.toString(id), text);
 			documents.add(doc);
-			System.out.println("Insert id: " + lyricId + ", text.length(): " + text.length());
+			System.out.println("Insert id: " + id + ", text.length(): " + text.length());
 		}
 		
 		/* A controller to manage the processing pipeline. */
@@ -52,7 +59,19 @@ public class LyricClustering {
 		ConsoleFormatter.displayResults(byTopicClusters);
 		
 		for (Cluster cluster : clustersByTopic) {
-			//cluster.
+			System.out.println("Label: " + cluster.getLabel());
+			System.out.println("Id: " + cluster.getId());
+			
+			insertClusterStatement.setInt(1, cluster.getId());
+			insertClusterStatement.setString(2, cluster.getLabel());
+			insertClusterStatement.execute();
+			
+			for (Document member : cluster.getDocuments()) {
+				insertClusterMemberStatement.setInt(1, Integer.parseInt(member.getTitle()));
+				insertClusterMemberStatement.setInt(2, cluster.getId());
+				insertClusterMemberStatement.addBatch();
+			}
+			insertClusterMemberStatement.executeBatch();
 		}
 	}
 
