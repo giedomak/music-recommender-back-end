4cc5d0d844c2b67715d0eb7470054e850e0133e1
Marichelle <marichelleg@gmail.com>
2014-10-17 12:10:38 +0200

    title clustering

---
+++ src/termclustering/TitleClustering.java
@@ -0,0 +1,82 @@
+package termclustering;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.carrot2.clustering.lingo.LingoClusteringAlgorithm;
+import org.carrot2.clustering.synthetic.ByUrlClusteringAlgorithm;
+import org.carrot2.core.Cluster;
+import org.carrot2.core.Controller;
+import org.carrot2.core.ControllerFactory;
+import org.carrot2.core.Document;
+import org.carrot2.core.ProcessingResult;
+
+public class TitleClustering {
+	final ArrayList<Document> documents = new ArrayList<Document>();
+	
+	public TitleClustering() throws SQLException {
+		
+		org.apache.log4j.BasicConfigurator.configure();
+		
+		Connection connection;
+		connection = DriverManager.getConnection("jdbc:mysql://178.62.207.179/2id26?"
+				+ "user=root&password=Aarde-Rond-1");
+		
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery("SELECT id, title FROM song");
+		
+		PreparedStatement insertClusterStatement = connection
+		          .prepareStatement("INSERT INTO titlecluster VALUES (?, ?, null)");
+		
+		PreparedStatement insertClusterMemberStatement = connection
+		          .prepareStatement("INSERT INTO titleclustermember VALUES (?, ?)");
+		
+		while (resultSet.next()) {
+			int id = resultSet.getInt("id");
+			String title = resultSet.getString("title").toLowerCase();
+			Document doc = new Document(Integer.toString(id), title);
+			documents.add(doc);
+			System.out.println("Insert id: " + id + "," + title + " ");
+		}
+		
+		/* A controller to manage the processing pipeline. */
+		final Controller controller = ControllerFactory.createSimple();
+		 
+		/*
+		* Perform clustering by topic using the Lingo algorithm. Lingo can
+		* take advantage of the original query, so we provide it along with the documents.
+		*/
+		final ProcessingResult byTopicClusters = controller.process(documents, null,
+		LingoClusteringAlgorithm.class);
+		final List<Cluster> clustersByTopic = byTopicClusters.getClusters();
+		
+		ConsoleFormatter.displayResults(byTopicClusters);
+		
+		for (Cluster cluster : clustersByTopic) {
+			System.out.println("Label: " + cluster.getLabel());
+			System.out.println("Id: " + cluster.getId());
+			
+			insertClusterStatement.setInt(1, cluster.getId());
+			insertClusterStatement.setString(2, cluster.getLabel());
+			insertClusterStatement.execute();
+			
+			for (Document member : cluster.getDocuments()) {
+				insertClusterMemberStatement.setInt(1, Integer.parseInt(member.getTitle()));
+				insertClusterMemberStatement.setInt(2, cluster.getId());
+				insertClusterMemberStatement.addBatch();
+			}
+			insertClusterMemberStatement.executeBatch();
+		}
+	}
+
+	public static void main(String[] args) throws SQLException {
+		new TitleClustering();
+	}
+
+}
---
+++ src/termclustering/TitleClusteringDatabaseLucene.java
@@ -0,0 +1,164 @@
+package termclustering;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.IntField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
+import org.carrot2.clustering.lingo.LingoClusteringAlgorithm;
+import org.carrot2.core.Controller;
+import org.carrot2.core.ControllerFactory;
+import org.carrot2.core.ProcessingComponentConfiguration;
+import org.carrot2.core.ProcessingResult;
+import org.carrot2.core.attribute.CommonAttributesDescriptor;
+import org.carrot2.source.lucene.LuceneDocumentSource;
+import org.carrot2.source.lucene.LuceneDocumentSourceDescriptor;
+import org.carrot2.source.lucene.SimpleFieldMapperDescriptor;
+
+import com.google.common.collect.Maps;
+
+public class TitleClusteringDatabaseLucene {
+	// based on http://www.lucenetutorial.com/lucene-in-5-minutes.html
+	// based on https://github.com/carrot2/carrot2/blob/master/applications/carrot2-examples/examples/org/carrot2/examples/clustering/ClusteringDataFromLucene.java
+	
+	public Directory index;
+	public IndexWriterConfig config;
+	public StandardAnalyzer analyzer;
+	
+	public TitleClusteringDatabaseLucene() throws SQLException, IOException, ParseException {	
+		analyzer = new StandardAnalyzer();
+		index = new RAMDirectory();
+		config = new IndexWriterConfig(Version.LUCENE_4_10_1, analyzer);
+	}
+
+	public static void main(String[] args) throws SQLException, IOException, ParseException {
+		TitleClusteringDatabaseLucene database = new TitleClusteringDatabaseLucene();
+	
+		database.fillFromMysql();
+		
+	    // Search query
+	    String querystr = "love";
+
+	    Query q = new QueryParser("title", database.analyzer).parse(querystr);
+	    //database.search(q);
+	    
+	    org.apache.log4j.BasicConfigurator.configure();
+	    
+	    database.clusterTitle();
+	}
+
+	private void clusterTitle() {
+		final Controller controller = ControllerFactory.createPooling();
+		
+        final Map<String, Object> luceneGlobalAttributes = new HashMap<String, Object>();
+
+        LuceneDocumentSourceDescriptor
+            .attributeBuilder(luceneGlobalAttributes)
+            .directory(index)
+            .analyzer(analyzer);
+
+        /*
+         * Specify fields providing data inside your Lucene index.
+         */
+        SimpleFieldMapperDescriptor
+            .attributeBuilder(luceneGlobalAttributes)
+            .titleField("id");
+            //.titleField("title") // TODO: add to index
+           // .contentField("lyric"); ERUIT GEHAALD MG
+            //.searchFields(Arrays.asList(new String [] {"titleField", "fullContent"}));
+		
+        controller.init(new HashMap<String, Object>(),
+            new ProcessingComponentConfiguration(LuceneDocumentSource.class, "lucene",
+                luceneGlobalAttributes));
+
+        /*
+         * Perform processing.
+         */
+        //String query = "mining";
+        String query = "me";
+        final Map<String, Object> processingAttributes = Maps.newHashMap();
+        CommonAttributesDescriptor.attributeBuilder(processingAttributes).query(query).results(1000);
+
+        /*
+         * We need to refer to the Lucene component by its identifier we set during
+         * initialization. As we've not assigned any identifier to the
+         * LingoClusteringAlgorithm we want to use, we can its fully qualified class name.
+         */
+        ProcessingResult process = controller.process(processingAttributes, "lucene",
+            LingoClusteringAlgorithm.class);
+        
+        ConsoleFormatter.displayResults(process);
+	}
+
+	public void search(Query q)
+			throws IOException {
+		// 3. search
+	    int hitsPerPage = 10;
+	    IndexReader reader = DirectoryReader.open(index);
+	    IndexSearcher searcher = new IndexSearcher(reader);
+	    TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage, true);
+	    searcher.search(q, collector);
+	    ScoreDoc[] hits = collector.topDocs().scoreDocs;
+	    
+	    // 4. display results
+	    System.out.println("Found " + hits.length + " hits.");
+	    for(int i=0;i<hits.length;++i) {
+	      int docId = hits[i].doc;
+	      Document d = searcher.doc(docId);
+	      System.out.println((i + 1) + ". " + d.get("id") + "\t" + d.get("title"));
+	    }
+
+	    // reader can only be closed when there
+	    // is no need to access the documents any more.
+	    reader.close();
+	}
+
+	public void fillFromMysql()
+			throws SQLException, IOException {
+		Connection connection;
+		connection = DriverManager.getConnection("jdbc:mysql://178.62.207.179/2id26?"
+				+ "user=root&password=Aarde-Rond-1");
+		
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery("SELECT songId, title FROM song LIMIT 1000");
+		
+		IndexWriter w = new IndexWriter(index, config);
+		
+		while (resultSet.next()) {
+			int songId = resultSet.getInt("id");
+			String title = resultSet.getString("title").toLowerCase();
+			addTitle(w, title, songId);
+			System.out.println("Insert id: " + songId + ", " + title + "");
+		}
+		w.close();
+	}
+	
+	private static void addTitle(IndexWriter w, String title, int songId) throws IOException {
+		  Document doc = new Document();
+		  doc.add(new TextField("string", title, Field.Store.YES));
+		  doc.add(new IntField("id", songId, Field.Store.YES));
+		  w.addDocument(doc);
+	}
+}
